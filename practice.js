5c758356a719b9d5239c92166d900da06637c860
hwangbohyun <hbhyun3102@naver.com>
2025-01-30 22:10:58 +0900

    자바스크립트 중급 Math 매소드 학습

--- practice.js
+++ practice.js
@@ -1,14 +1,4 @@
-const myLib = {
-  config: {
-    theme: "dark",
-    lang: "ko",
-  },
-};
-
-// myLib.config.theme = "light"; // 기존 값이 덮어씌어짐
-
-const uniqueKey = Symbol("theme");
-myLib.config[uniqueKey] = "custom-theme";
-
-console.log(myLib.config.theme); // dark
-console.log(myLib.config[uniqueKey]); // custom-theme
+let x = Number("x");
+console.log(x == NaN);
+console.log(x === NaN);
+console.log(NaN == NaN);
