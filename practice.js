c4971f9549f07e520492e007bcb7abd83e7d3d2a
hwangbohyun <hbhyun3102@naver.com>
2025-01-31 16:06:27 +0900

    자바스크립트 중급 String 메소드 학습 완료

--- practice.js
+++ practice.js
@@ -1,4 +1,16 @@
-let x = Number("x");
-console.log(x == NaN);
-console.log(x === NaN);
-console.log(NaN == NaN);
+//금칙어 : 콜라
+//includes
+//문자가 있으면 true
+//문자가 없으면 false 반환환
+
+function hasCola(str) {
+  if (str.includes("콜라")) {
+    console.log("금칙어가 존재한다");
+  } else {
+    console.log("통과");
+  }
+}
+
+hasCola("왕 사이다가 최고야");
+hasCola("무슨소리 콜라가 더 맛있어");
+hasCola("콜라가 짱이야 콜라가 짱");
