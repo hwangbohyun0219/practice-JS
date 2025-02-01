9a6a325f2995584fafdd607c67af5559e579b46f
hwangbohyun <hbhyun3102@naver.com>
2025-02-01 21:55:56 +0900

    자바스크립트 중급 arr.reduce() arr.map() 등 메소등 학습 완료

--- practice.js
+++ practice.js
@@ -1,11 +1,16 @@
 let userList = [
   { name: "Mike", age: 30 },
-  { name: "Jane", age: 27 },
   { name: "Tom", age: 10 },
+  { name: "Jane", age: 27 },
+  { name: "Sue", age: 26 },
+  { name: "Harry", age: 43 },
+  { name: "Steve", age: 60 },
 ];
 
-let newUSerList = userList.map((user, index, userList) => {
-  return Object.assign({}, user, { isAdult: user.age > 19, id: index + 1 });
-});
-console.log(newUSerList);
-console.log(userList);
+let result = userList.reduce((prev, cur) => {
+  if (userList.name.length === 3) {
+    prev.push(cur);
+  }
+  return prev;
+}, []);
+console.log(result);
