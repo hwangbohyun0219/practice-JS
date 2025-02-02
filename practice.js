41bc68167fd04d39edcc6064c51b7992a8bd1c8a
hwangbohyun <hbhyun3102@naver.com>
2025-02-02 23:23:29 +0900

    자바스크립트 중급 나머지 매개변 학습 완료

--- practice.js
+++ practice.js
@@ -1,54 +1,52 @@
+//나머지 매개변수, 전개구문
+//기초
+function showName(name) {
+  console.log(name);
+}
+showName("Mike", "Tom"); //Mike
+showName("Mike"); //Mike
+showName(); // undefined
+//arguments
+function showName1(name) {
+  console.log(arguments);
+  console.log(arguments.length);
+  console.log(arguments[0]);
+  console.log(arguments[1]);
+}
+showName1("Andy", "Jane");
+//나머지 매개변수
+function showNames2(...names) {
+  console.log(names);
+}
+showNames2();
+showNames2("Mike");
+showNames2("Mike", "Jane");
+//예시 reduce
 function add(...numbers) {
-  let result = 0;
-  numbers.forEach((num) => {
-    result += num;
-  });
+  let result = numbers.reduce((prev, cur) => {
+    return prev + cur;
+  }, 0);
   console.log(result);
 }
 add(1, 2, 3);
 add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
-// //전개구문 : 복제
-let arr = [1, 2, 3];
-let arr2 = [...arr];
-console.log(arr2);
-
-// let user = { name: "Mike", age: 34 };
-// let user2 = { ...user };
-// console.log(user2);
-// user2.name = "Tom";
-// console.log(user2);
-
-//방법1
-let arr1 = [1, 2, 3];
-let arr3 = [4, 5, 6];
-arr3.reverse().forEach((num) => {
-  arr1.unshift(num);
-});
-console.log(arr1);
-
-//방법2
-arr1 = [...arr3, ...arr1];
-console.log(arr1);
-
-//객체에서도 사용해보자
-//방법1
-let user = { name: "Mike" };
-let info = { age: 30 };
-let fe = ["JS", "React"];
-let lang = ["Korean", "English"];
-
-user = Object.assign({}, user, info, {
-  skills: [],
-});
-fe.forEach((item) => {
-  user.skills.push(item);
-});
-lang.forEach((item) => {
-  user.skills.push(item);
-});
-console.log(user);
-
-//방법2
-user = { ...user, ...info, skills: [...fe, ...lang] };
-console.log(user);
+//또 다른 방법 forEach
+function add1(...numbers) {
+  let result = 0;
+  numbers.forEach((num) => (result += num));
+  console.log(result);
+}
+add1(1, 2, 3);
+add1(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+//예시 user 객체를 만들어 주는 생성자 함수 만들기
+function User(name, age, ...skills) {
+  this.name = name;
+  this.age = age;
+  this.skills = skills;
+}
+const user1 = new User("Mike", 30, "html", "css");
+const user2 = new User("Jane", "25", "React");
+const user3 = new User("Tom", "10", "English");
+console.log(user1);
+console.log(user2);
+console.log(user3);
