7ee52d90998c148c0c89a3d3925f658b2721977c
hwangbohyun <hbhyun3102@naver.com>
2025-02-02 15:24:10 +0900

    자바스크립트 중급 구조분해할당 학습 완료

--- practice.js
+++ practice.js
@@ -1,16 +1,49 @@
-let userList = [
-  { name: "Mike", age: 30 },
-  { name: "Tom", age: 10 },
-  { name: "Jane", age: 27 },
-  { name: "Sue", age: 26 },
-  { name: "Harry", age: 43 },
-  { name: "Steve", age: 60 },
-];
+let [x, y] = [1, 2];
+console.log(x);
+console.log(y);
 
-let result = userList.reduce((prev, cur) => {
-  if (userList.name.length === 3) {
-    prev.push(cur);
-  }
-  return prev;
-}, []);
-console.log(result);
+let users = ["Mike", "Jane", "Tom"];
+let [user1, user2, user3] = users;
+console.log(user1);
+console.log(user2);
+console.log(user3);
+
+let str = "Mike-Tom-Jane";
+let [user4, user5, user6] = str.split("-");
+console.log(str.split("-"));
+console.log(user4);
+console.log(user5);
+console.log(user6);
+
+let [user7, , user8] = ["Mike", "Jane", "Tom", "Tony"];
+console.log(user7);
+console.log(user8);
+
+let a = 1;
+let b = 2;
+[a, b] = [b, a];
+console.log([a, b]);
+
+let user = { name: "Mike", age: 30 };
+// let { name, age } = user;
+// console.log(name);
+// console.log(age);
+
+// let { name: userName, age: userAge } = user;
+// console.log(userName);
+// console.log(userAge);
+
+// let { name, age, gender } = user;
+// console.log({ name, age, gender });
+
+// let { name, age, gender = "male" } = user;
+// console.log({ name, age, gender });
+
+let king = {
+  name: "Jane",
+  age: 45,
+  gender: "female",
+};
+
+let { name, age, gender = "male" } = king;
+console.log({ name, age, gender });
