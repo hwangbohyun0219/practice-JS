839f928bed645916dc9b3dcb2ea0d1c33878fbb2
hwangbohyun <hbhyun3102@naver.com>
2025-02-03 13:15:36 +0900

    자바스크립트 중급 전개 구문 학습 완료

--- practice.js
+++ practice.js
@@ -1,52 +1,49 @@
-//나머지 매개변수, 전개구문
-//기초
-function showName(name) {
-  console.log(name);
-}
-showName("Mike", "Tom"); //Mike
-showName("Mike"); //Mike
-showName(); // undefined
-//arguments
-function showName1(name) {
-  console.log(arguments);
-  console.log(arguments.length);
-  console.log(arguments[0]);
-  console.log(arguments[1]);
-}
-showName1("Andy", "Jane");
-//나머지 매개변수
-function showNames2(...names) {
-  console.log(names);
-}
-showNames2();
-showNames2("Mike");
-showNames2("Mike", "Jane");
-//예시 reduce
-function add(...numbers) {
-  let result = numbers.reduce((prev, cur) => {
-    return prev + cur;
-  }, 0);
-  console.log(result);
-}
-add(1, 2, 3);
-add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-//또 다른 방법 forEach
-function add1(...numbers) {
-  let result = 0;
-  numbers.forEach((num) => (result += num));
-  console.log(result);
-}
-add1(1, 2, 3);
-add1(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-//예시 user 객체를 만들어 주는 생성자 함수 만들기
-function User(name, age, ...skills) {
-  this.name = name;
-  this.age = age;
-  this.skills = skills;
-}
-const user1 = new User("Mike", 30, "html", "css");
-const user2 = new User("Jane", "25", "React");
-const user3 = new User("Tom", "10", "English");
-console.log(user1);
-console.log(user2);
-console.log(user3);
+//전개구문(Spread syntax)
+//1. 배열
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let result1 = [...arr1, ...arr2];
+console.log(result1);
+let result2 = [0, ...arr1, ...arr2, 7, 8, 9];
+console.log(result2);
+//2.객체
+let user = { name: "Mike" };
+let mike = { ...user, age: 20 };
+console.log(mike);
+//3.복제
+let arr3 = [1, 2, 3];
+let arr4 = [...arr3];
+console.log(arr4);
+let user1 = { name: "Mike", age: 20 };
+let user2 = { ...user };
+user2.name = "Tom";
+console.log(user1.name);
+console.log(user2.name);
+//예제 : [4,5,6,1,2,3]으로 만들어봐라
+let arr5 = [1, 2, 3];
+let arr6 = [4, 5, 6];
+arr5 = [...arr6, ...arr5];
+console.log(arr5);
+//원래라면 >>틀린방법
+arr6.reverse().forEach((num) => {
+  arr5.unshift(num);
+});
+console.log(arr5); //[6,5,4,3,2,1]이 출력되어 이 방법으로 불가능 따라서 reverse를 활용해야
+//예제 : 객체
+let human = { name: "mike" };
+let info = { age: 30 };
+let fe = ["JS", "React"];
+let lang = ["Korean", "English"];
+human = Object.assign({}, human, info, {
+  skills: [],
+});
+fe.forEach((item) => {
+  human.skills.push(item);
+});
+lang.forEach((item) => {
+  human.skills.push(item);
+});
+console.log(human);
+//전개구문
+human = { ...human, ...info, skills: [...fe, ...lang] };
+console.log(human);
