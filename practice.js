f92661fd0725d6e2c831ff2ad6030dc75ab2c8a4
hwangbohyun <hbhyun3102@naver.com>
2025-02-03 21:14:28 +0900

    자바스크립트 중급 setTimeout/setInterval 학습 완료

--- practice.js
+++ practice.js
@@ -1,56 +1,31 @@
-//클로저(Closure)
-//렉시컬 환경 예시
-function outer1() {
-  let count = 0;
-  return function inner() {
-    count++;
-    console.log(count);
-  };
+//setTimeout, setinterval
+//setTimeout
+function fn() {
+  console.log(3);
 }
-const counter = outer1();
-counter();
-counter();
-counter();
-//스코프 체인 예시
-let a = 10;
-function outer() {
-  let b = 20;
+setTimeout(fn, 3000);
 
-  function inner() {
-    let c = 30;
-    console.log(a, b, c);
+setTimeout(function fn() {
+  console.log(3);
+}, 3000);
+
+function showName(name) {
+  console.log(name);
+}
+const tId = setTimeout(showName, 3000, "Mike");
+clearTimeout(tId);
+//setinterval
+function showName1(name) {
+  console.log(name);
+}
+const tId1 = setInterval(showName1, 3000, "Jane");
+clearInterval(tId1);
+//예시
+let num = 0;
+function showTime() {
+  console.log(`안녕하세요. 접속하신지 ${num++}이 지났습니다.`);
+  if (num > 5) {
+    clearInterval(tId2);
   }
-  inner();
 }
-outer();
-//클로저를 이용하여 계좌 입출금 만들기
-function outer() {
-  let total = 10000;
-  return function inner(getMoney) {
-    total += getMoney;
-    console.log(
-      `안녕하세요. ${getMoney}원 입급되셨고, 총 액은 ${total}입니다.`
-    );
-  };
-}
-const tId = outer();
-tId(2000);
-tId(3000);
-//비밀번호 확인하는 기능 만들기
-function createPasswordChecker() {
-  let realPassword = 1234;
-  return function checkPassword(password) {
-    if (realPassword == password) {
-      console.log(
-        `입력된 비밀번호는 ${password}입니다. 비밀번호와 일치합니다.`
-      );
-    } else {
-      console.log(
-        `입력된 비밀번호는 ${password}입니다. 비밀번호가 불일치합니다.`
-      );
-    }
-  };
-}
-const check = createPasswordChecker();
-check(1234);
-check(234);
+const tId2 = setInterval(showTime, 1000);
