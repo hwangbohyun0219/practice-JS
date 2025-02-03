3de42e1e283aa92d9d4784aa02cf6b6c313cad56
hwangbohyun <hbhyun3102@naver.com>
2025-02-03 20:06:36 +0900

    자바스크립트 중급 클로저 학습 완료

--- practice.js
+++ practice.js
@@ -1,49 +1,56 @@
-//전개구문(Spread syntax)
-//1. 배열
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
-let result1 = [...arr1, ...arr2];
-console.log(result1);
-let result2 = [0, ...arr1, ...arr2, 7, 8, 9];
-console.log(result2);
-//2.객체
-let user = { name: "Mike" };
-let mike = { ...user, age: 20 };
-console.log(mike);
-//3.복제
-let arr3 = [1, 2, 3];
-let arr4 = [...arr3];
-console.log(arr4);
-let user1 = { name: "Mike", age: 20 };
-let user2 = { ...user };
-user2.name = "Tom";
-console.log(user1.name);
-console.log(user2.name);
-//예제 : [4,5,6,1,2,3]으로 만들어봐라
-let arr5 = [1, 2, 3];
-let arr6 = [4, 5, 6];
-arr5 = [...arr6, ...arr5];
-console.log(arr5);
-//원래라면 >>틀린방법
-arr6.reverse().forEach((num) => {
-  arr5.unshift(num);
-});
-console.log(arr5); //[6,5,4,3,2,1]이 출력되어 이 방법으로 불가능 따라서 reverse를 활용해야
-//예제 : 객체
-let human = { name: "mike" };
-let info = { age: 30 };
-let fe = ["JS", "React"];
-let lang = ["Korean", "English"];
-human = Object.assign({}, human, info, {
-  skills: [],
-});
-fe.forEach((item) => {
-  human.skills.push(item);
-});
-lang.forEach((item) => {
-  human.skills.push(item);
-});
-console.log(human);
-//전개구문
-human = { ...human, ...info, skills: [...fe, ...lang] };
-console.log(human);
+//클로저(Closure)
+//렉시컬 환경 예시
+function outer1() {
+  let count = 0;
+  return function inner() {
+    count++;
+    console.log(count);
+  };
+}
+const counter = outer1();
+counter();
+counter();
+counter();
+//스코프 체인 예시
+let a = 10;
+function outer() {
+  let b = 20;
+
+  function inner() {
+    let c = 30;
+    console.log(a, b, c);
+  }
+  inner();
+}
+outer();
+//클로저를 이용하여 계좌 입출금 만들기
+function outer() {
+  let total = 10000;
+  return function inner(getMoney) {
+    total += getMoney;
+    console.log(
+      `안녕하세요. ${getMoney}원 입급되셨고, 총 액은 ${total}입니다.`
+    );
+  };
+}
+const tId = outer();
+tId(2000);
+tId(3000);
+//비밀번호 확인하는 기능 만들기
+function createPasswordChecker() {
+  let realPassword = 1234;
+  return function checkPassword(password) {
+    if (realPassword == password) {
+      console.log(
+        `입력된 비밀번호는 ${password}입니다. 비밀번호와 일치합니다.`
+      );
+    } else {
+      console.log(
+        `입력된 비밀번호는 ${password}입니다. 비밀번호가 불일치합니다.`
+      );
+    }
+  };
+}
+const check = createPasswordChecker();
+check(1234);
+check(234);
