c1743a8c625587505e884d06e5a4007b9261f76d
hwangbohyun <hbhyun3102@naver.com>
2025-02-10 21:16:29 +0900

    자바스크립트 중급 클래스 학습

--- practice.js
+++ practice.js
@@ -1,57 +1,85 @@
-//상속과 프로토타입
-const car = {
-  wheels: 4,
-  drive() {
-    console.log("drive..");
-  },
+// //Promise
+// //callback hell 현상
+// const f1 = (callback) => {
+//   setTimeout(function () {
+//     console.log("1번 주문 완료");
+//     callback();
+//   }, 1000);
+// };
+// const f2 = (callback) => {
+//   setTimeout(function () {
+//     console.log("2번 주문 완료");
+//     callback();
+//   }, 3000);
+// };
+// const f3 = (callback) => {
+//   setTimeout(function () {
+//     console.log("3번 주문 완료");
+//     callback();
+//   }, 2000);
+// };
+// console.log("시작");
+// f1(function () {
+//   f2(function () {
+//     f3(function () {
+//       console.log("끝");
+//     });
+//   });
+// });
+//Promise 체이닝 적용
+const f11 = () => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("1번 주문 완료");
+    }, 1000);
+  });
 };
-const bmw = {
-  color: "red",
-  navigation: 1,
+const f22 = (message) => {
+  console.log(message);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("2번 주문 완료");
+    }, 3000);
+  });
+};
+const f33 = (message) => {
+  console.log(message);
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("3번 주문 완료");
+    }, 2000);
+  });
 };
 
-const benz = {
-  color: "black",
-};
-
-const audi = {
-  color: "blue",
-};
-
-const x5 = {
-  color: "white",
-  name: "x5",
-};
-
-bmw.__proto__ = car;
-benz.__proto__ = car;
-audi.__proto__ = car;
-console.log(bmw);
-
-x5.__proto__ = bmw;
-//for문으로 반복 돌려보기
-for (p in x5) {
-  console.log(p);
-  console.log(x5[p]);
+// console.log("시작");
+// f11()
+//   .then((res) => f22(res))
+//   .then((res) => f33(res))
+//   .then((res) => console.log(res))
+//   .catch(console.log)
+//   .finally(() => {
+//     console.log("끝");
+//   });
+console.log("시작");
+async function order() {
+  const result1 = await f11();
+  const result2 = await f22(result1);
+  const result3 = await f33(result2);
+  console.log(result3);
+  console.log("종료");
 }
-console.log(x5);
-console.log(Object.keys(x5));
-console.log(Object.values(x5));
-//생성자 함수를 이용하여 객체를 만들고 프로토타입 이용해보기
-const bmw1 = function (color) {
-  this.color = color;
-};
-
-bmw1.prototype.wheels = 4;
-bmw1.prototype.drive = function () {
-  console.log("drive~");
-};
-
-const x51 = new bmw1("red");
-const z4 = new bmw1("blue");
-
-console.log(x51.wheels);
-console.log(z4);
-console.log(z4 instanceof bmw1);
-console.log(z4.constructor === bmw1);
-//코드를 더 간단하게 정리하는 방법
+order();
+//async await
+function getName(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(name);
+    }, 1000);
+  });
+}
+async function showName() {
+  const result = await getName("Mike");
+  console.log(result);
+}
+console.log("시작");
+showName();
