e088521924c1809b2c946d4400ac43f0f3ed3737
hwangbohyun <hbhyun3102@naver.com>
2025-02-11 21:35:31 +0900

    자바스크립트 중급 클래스 학습 완료

--- practice.js
+++ practice.js
@@ -1,85 +1,58 @@
-// //Promise
-// //callback hell 현상
-// const f1 = (callback) => {
-//   setTimeout(function () {
-//     console.log("1번 주문 완료");
-//     callback();
-//   }, 1000);
-// };
-// const f2 = (callback) => {
-//   setTimeout(function () {
-//     console.log("2번 주문 완료");
-//     callback();
-//   }, 3000);
-// };
-// const f3 = (callback) => {
-//   setTimeout(function () {
-//     console.log("3번 주문 완료");
-//     callback();
-//   }, 2000);
-// };
-// console.log("시작");
-// f1(function () {
-//   f2(function () {
-//     f3(function () {
-//       console.log("끝");
-//     });
-//   });
-// });
-//Promise 체이닝 적용
-const f11 = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res("1번 주문 완료");
-    }, 1000);
-  });
+//class
+//생성자 함수
+const User = function (name, age) {
+  this.name = name;
+  this.age = age;
+  this.showName = function () {
+    console.log(this.name);
+  };
 };
-const f22 = (message) => {
-  console.log(message);
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res("2번 주문 완료");
-    }, 3000);
-  });
-};
-const f33 = (message) => {
-  console.log(message);
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res("3번 주문 완료");
-    }, 2000);
-  });
-};
+const Mike = new User("Mike", 30);
+//클래스로도 만들 수 있다.
+class User2 {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  showName() {
+    console.log(this.name);
+  }
+}
+const Tom = new User2("Tom", 19);
+console.log(Mike);
+console.log(Tom);
 
-// console.log("시작");
-// f11()
-//   .then((res) => f22(res))
-//   .then((res) => f33(res))
-//   .then((res) => console.log(res))
-//   .catch(console.log)
-//   .finally(() => {
-//     console.log("끝");
-//   });
-console.log("시작");
-async function order() {
-  const result1 = await f11();
-  const result2 = await f22(result1);
-  const result3 = await f33(result2);
-  console.log(result3);
-  console.log("종료");
+for (const p in Mike) {
+  console.log(p);
 }
-order();
-//async await
-function getName(name) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(name);
-    }, 1000);
-  });
+for (const j in Tom) {
+  console.log(j);
 }
-async function showName() {
-  const result = await getName("Mike");
-  console.log(result);
+//상속, extends
+class Car {
+  constructor(color) {
+    this.color = color;
+    this.wheels = 4;
+  }
+  drive() {
+    console.log("drive..");
+  }
+  stop() {
+    console.log("STOP!");
+  }
 }
-console.log("시작");
-showName();
+
+class Bmw extends Car {
+  stop() {
+    super.stop();
+    console.log("OFF");
+  }
+  park() {
+    console.log("PARK");
+  }
+}
+
+const z4 = new Bmw("blue");
+console.log(z4);
+console.log(z4.drive());
+console.log(z4.stop());
