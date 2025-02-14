4083a92ddc571e9a10128660dd1f6961f5e2fef8
hwangbohyun <hbhyun3102@naver.com>
2025-02-14 23:25:39 +0900

    자바스크립트 중급 generator 학습

--- practice.js
+++ practice.js
@@ -1,68 +1,16 @@
-//async-await
-//async
-async function getName() {
-  throw new Error("err...");
+//Generator
+function* fn() {
+  console.log(1);
+  yield 1;
+  console.log(2);
+  yield 2;
+  console.log(3);
+  console.log(4);
+  yield 3;
+  return "finish";
 }
-getName().then((err) => {
-  console.log(err);
-});
-//await
-function getName(name) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(name);
-    }, 1000);
-  });
-}
-async function showName() {
-  const result = await getName("Mike");
-  console.log(result);
-}
-console.log("시작");
-showName();
-//promise -> async/await
-const f1 = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res("1번 주문 완료");
-    }, 1000);
-  });
-};
-const f2 = (message) => {
-  console.log(message);
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res("2번 주문 완료");
-    }, 3000);
-  });
-};
-const f3 = (message) => {
-  console.log(message);
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res("3번 주문 완료");
-    }, 2000);
-  });
-};
 
-// console.log("시작");
-// f1()
-//   .then((res) => f2(res))
-//   .then((res) => f3(res))
-//   .then((res) => console.log(res))
-//   .catch(console.log)
-//   .finally(() => {
-//     console.log("끝");
-//   });
-
-console.log("시작");
-async function order() {
-  try {
-    const result = await Promise.all([f1(), f2(), f3()]);
-    console.log(result);
-  } catch (e) {
-    console.log(e);
-  }
-  console.log("종료");
-}
-order();
+const a = fn();
+console.log(a.next());
+console.log(a.next());
+console.log(a.return("end"));
