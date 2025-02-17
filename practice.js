fae60c5698ad6e9f7a480be7cd0080066df053f8
hwangbohyun <hbhyun3102@naver.com>
2025-02-17 14:42:53 +0900

    자바스크립트 중급 generator 학습 완료

--- practice.js
+++ practice.js
@@ -1,16 +1,75 @@
 //Generator
+const arr = [1, 2, 3, 4, 5];
+console.log(arr);
+const it = arr[Symbol.iterator]();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+for (let num of arr) {
+  console.log(num);
+}
+
 function* fn() {
-  console.log(1);
-  yield 1;
-  console.log(2);
-  yield 2;
-  console.log(3);
-  console.log(4);
-  yield 3;
-  return "finish";
+  yield 4;
+  yield 5;
+  yield 6;
 }
 
 const a = fn();
-console.log(a.next());
-console.log(a.next());
-console.log(a.return("end"));
+
+console.log(a[Symbol.iterator]() === a);
+for (let num of a) {
+  console.log(num);
+}
+
+//문자열도 되는지 확인해보기
+const str = "hello";
+const xx = str[Symbol.iterator]();
+
+for (let s of xx) {
+  console.log(s);
+}
+
+function* fn() {
+  const num1 = yield "첫번째 숫자를 입력해주세요";
+  console.log(num1);
+
+  const num2 = yield "두번째 숫자를 입력해주세요";
+  console.log(num2);
+
+  return num1 + num2;
+}
+
+const b = fn();
+console.log(b.next());
+console.log(b.next(3));
+console.log(b.next(4));
+
+function* fn5() {
+  let index = 0;
+  while (true) {
+    yield index++;
+  }
+}
+const c = fn5();
+console.log(c.next());
+console.log(c.next());
+
+//다른 generator함수 호출하기
+function* gen1() {
+  yield "w";
+  yield "o";
+  yield "r";
+  yield "l";
+  yield "d";
+}
+function* gen2() {
+  yield "Hello,";
+  yield* gen1();
+  yield "!";
+}
+console.log(...gen2());
