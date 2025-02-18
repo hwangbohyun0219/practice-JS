a96b04b535ff2ba8dd7fcdf00e73856dc8781cc1
hwangbohyun <hbhyun3102@naver.com>
2025-02-18 20:07:49 +0900

    자바스크립트 중급 복습1

--- practice.js
+++ practice.js
@@ -1,75 +1,11 @@
-//Generator
-const arr = [1, 2, 3, 4, 5];
-console.log(arr);
-const it = arr[Symbol.iterator]();
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-
-for (let num of arr) {
-  console.log(num);
-}
-
-function* fn() {
-  yield 4;
-  yield 5;
-  yield 6;
-}
-
-const a = fn();
-
-console.log(a[Symbol.iterator]() === a);
-for (let num of a) {
-  console.log(num);
-}
-
-//문자열도 되는지 확인해보기
-const str = "hello";
-const xx = str[Symbol.iterator]();
-
-for (let s of xx) {
-  console.log(s);
-}
-
-function* fn() {
-  const num1 = yield "첫번째 숫자를 입력해주세요";
-  console.log(num1);
-
-  const num2 = yield "두번째 숫자를 입력해주세요";
-  console.log(num2);
-
-  return num1 + num2;
-}
-
-const b = fn();
-console.log(b.next());
-console.log(b.next(3));
-console.log(b.next(4));
-
-function* fn5() {
-  let index = 0;
-  while (true) {
-    yield index++;
-  }
-}
-const c = fn5();
-console.log(c.next());
-console.log(c.next());
-
-//다른 generator함수 호출하기
-function* gen1() {
-  yield "w";
-  yield "o";
-  yield "r";
-  yield "l";
-  yield "d";
-}
-function* gen2() {
-  yield "Hello,";
-  yield* gen1();
-  yield "!";
-}
-console.log(...gen2());
+//ES2021에 추가된 기능
+//String.replaceAll()
+const person = {
+  name: "홍길동",
+  age: 25,
+  sayHello: function () {
+    console.log(`안녕하세요. 저는 ${this.name}입니다.`);
+  },
+};
+console.log(person.name);
+person.sayHello();
